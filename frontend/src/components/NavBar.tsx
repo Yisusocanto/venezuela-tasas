b3b00e5c20d894c4b6a01e1e7494500e5dfc372d
Jesus Ocanto <yisusocanto1984@gmail.com>
2026-02-11 16:01:06 -0400

    app icon added and navbar improved.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,16 +1,40 @@
+import { linkVariants } from "@heroui/react";
+import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function NavBar() {
+  const pathname = usePathname();
+  const slots = linkVariants();
+
+  const links = [
+    { label: "Inicio", href: "/" },
+    { label: "Historial", href: "/historial" },
+  ];
+
   return (
-    <div className="w-full h-10 p-8 px-20 items-center border-b-accent-soft border-b flex justify-between ">
+    <div className="w-full h-10 p-8 px-20 items-center bg-background-secondary  border-b flex justify-between">
       <div className="flex items-center gap-2">
-        Logo
-        <h1 className="text-xl font-bold">Dolar Venezuela</h1>
+        <Image
+          src={"/dolar-venezuela-icon.png"}
+          alt="Dolar Venezuela icon."
+          width={40}
+          height={40}
+        />
+        <Link href={"/"} className="text-2xl font-bold">
+          Dolar Venezuela
+        </Link>
       </div>
-      <div className="flex gap-2">
-        <Link href={"/"}>Inicio</Link>
-        <Link href={"/historial"}>Historial</Link>
-        <Link href={"/api"}>API</Link>
+      <div className="flex gap-4">
+        {links.map((link) => (
+          <Link
+            key={link.label}
+            href={link.href}
+            className={`${slots.base()} text-lg ${pathname == link.href ? "decoration-accent" : ""}`}
+          >
+            {link.label}
+          </Link>
+        ))}
       </div>
     </div>
   );
